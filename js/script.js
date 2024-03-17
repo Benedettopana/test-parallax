a286ca7752fa1615b5f64afcff813783e4f34ae6
Benedettopana <benedetto.pana@gmail.com>
2024-03-18 00:58:38 +0100

    [add]fix

--- js/script.js
+++ js/script.js
@@ -21,11 +21,17 @@ createApp({
       //* Do alle posizioni delle img dei volori in base al value preso dallo scoll
       //? In base al tipo di img abbiamo dei moltiplicatori da applicare
       this.titleMarginTop = value * 1.2;
+      console.log("h1: ", this.titleMarginTop);
       this.leaf1MarginLeft = -value;
+      console.log("foglia 1: ", this.leaf1MarginLeft);
       this.leaf2MarginLeft = value;
+      console.log("foglia 2: ", this.leaf2MarginLeft);
       this.bush2MarginBottom = -value;
+      console.log("bush 2: ", this.bush2MarginBottom);
       this.mount1MarginBottom = -value * 1.2;
+      console.log("montagna 1: ", this.mount1MarginBottom);
       this.mount2MarginBottom = -value * 1.5;
+      console.log("montagna 2: ", this.mount2MarginBottom);
     },
   },
 
