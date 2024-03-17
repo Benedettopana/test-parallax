2d9e3605c3199fb68cea6a20757b12d73d2dcb01
Benedettopana <benedetto.pana@gmail.com>
2024-03-18 00:18:38 +0100

    [fix]animation

--- js/script.js
+++ js/script.js
@@ -20,12 +20,12 @@ createApp({
       console.log(value);
       //* Do alle posizioni delle img dei volori in base al value preso dallo scoll
       //? In base al tipo di img abbiamo dei moltiplicatori da applicare
-      this.titleMarginTop = value * 1.1;
+      this.titleMarginTop = value * 1.2;
       this.leaf1MarginLeft = -value;
       this.leaf2MarginLeft = value;
       this.bush2MarginBottom = -value;
-      this.mount1MarginBottom = -value * 1.1;
-      this.mount2MarginBottom = -value * 1.2;
+      this.mount1MarginBottom = -value * 1.2;
+      this.mount2MarginBottom = -value * 1.5;
     },
   },
 
